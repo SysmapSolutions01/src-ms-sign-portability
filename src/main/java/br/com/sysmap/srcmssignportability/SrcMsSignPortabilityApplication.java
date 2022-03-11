391685ad92bc6dfe24c2754b968defdbd6e23fa9
Anderson Florencio Conforto <anderson.conforto@sysmap.com.br>
2022-03-11 11:35:33 -0300

    first commit

---
+++ src/main/java/br/com/sysmap/srcmssignportability/SrcMsSignPortabilityApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.srcmssignportability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcMsSignPortabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrcMsSignPortabilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/sysmap/srcmssignportability/SrcMsSignPortabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.srcmssignportability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcMsSignPortabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
